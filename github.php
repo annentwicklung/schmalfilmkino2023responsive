826bdb9950b88f3e2228ee41252d7ba39afd8d3f
T. Riechert <thomas.riechert@htwk-leipzig.de>
2023-06-22 17:27:53 +0200

    now working

--- github.php
+++ github.php
@@ -1,4 +1,4 @@
-	<?php
+<?php
 /**
  * The main template file
  *
@@ -15,45 +15,25 @@
  * @version 1.0
  */
 
-
-
-$repo_owner = 'annentwicklung';
-$repo_name = 'schmalfilmkino2023responsive';
+$wp_theme='schmalfilmkino_2023-responsive';
 
 function getFileFromGitHub($file_path) {
-	global $repo_owner;
-	global $repo_name;
-	global $repo_token;
 
-	$api_url = 'https://api.github.com/repos/'.$repo_owner.'/'.$repo_name.'/contents'.$file_path;
-
-	echo $api_url;
-	$curl = curl_init();
-	curl_setopt_array($curl, [
-    CURLOPT_URL => $api_url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => [
-        "Authorization: Bearer {$repo_token}",
-        "Accept: application/vnd.github.v3.raw"
-    ]
-	]);
-
-	$response = curl_exec($curl);
-	$status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-	if ($status_code == 200) {
-    	return $response;
-	} else {
-    	return $status_code;
-	}
-	curl_close($curl);
 }
 
 $request = $_SERVER['REQUEST_URI'];
 $forward_to = str_replace('/wp-content/themes/'.$wp_theme,'',$request);
 
+$parts = explode('.', $forward_to);
+$filetype = strtolower(end($parts));
 
-echo getFileFromGitHub($forward_to);
+$api_url  = 'https://raw.githubusercontent.com/annentwicklung/schmalfilmkino2023responsive/main'.$forward_to;
+$response = file_get_contents($api_url);
+
+if ($filetype=='css') header('Content-Type: text/css');
+if ($filetype=='html') header('Content-Type: text/html');
+
+echo $response;
 
 //eval($response);
 
